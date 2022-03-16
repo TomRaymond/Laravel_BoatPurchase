a63099014f481bab0759351cd55aeb52ab2a882a
CaptainChuckleFuck <Raymond388@icloud.com>
2022-03-16 13:38:48 +0000

    Setup Testing
    - Removed example test files
    - Added UserTest files
    - Added feature tests for index and purchase routes

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_index_route_returns_200()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_purchase_route_returns_200()
+    {
+        $response = $this->get('/purchase');
+
+        $response->assertStatus(200);
+    }
+}
