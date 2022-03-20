eeb84dd221ec68416f9fa4bcaf1b7e1674546382
CaptainChuckleFuck <Raymond388@icloud.com>
2022-03-20 20:46:14 +0000

    Added workaround to make navbar burger work

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>{{ config('app.name', 'Boats"R"Us') }}</title>        
+         <!-- Scripts -->
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Styles -->
         <link href="https://bootswatch.com/5/flatly/bootstrap.min.css" rel="stylesheet">
         <!-- Fonts -->
